75c336e9cef295676ba4dfa3951b88e3de15bb5b
Harenabs21 <hei.harena.6@gmail.com>
2024-06-17 15:40:40 +0300

    feat(test): implementation des classes pour le test

--- src/main/java/com/std22049/Main.java
+++ src/main/java/com/std22049/Main.java
@@ -2,6 +2,6 @@ package com.std22049;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         System.out.println("Hello world");
     }
 }
---
+++ src/test/java/std22049/MarcheurTest.java
@@ -0,0 +1,70 @@
+package std22049;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.std22049.Carte;
+import com.std22049.Lieu;
+import com.std22049.Marcheur;
+import com.std22049.Rue;
+
+public class MarcheurTest {
+    
+    @Test
+    public void HEI_vers_ESTI(){
+        // Créer les lieux
+         Lieu HEI = new Lieu("HEI");
+         Lieu Pullman = new Lieu("Pullman");
+         Lieu Balancoire = new Lieu("Balancoire");
+         Lieu ESTI = new Lieu("ESTI");
+         Lieu NextA = new Lieu("NextA");
+         Lieu Boulevard = new Lieu("Boulevard de l'europe");
+         Lieu Marais = new Lieu("Marais");
+         Lieu Sekolintsika = new Lieu("Sekolintsika");
+ 
+         // Créer les rues
+         Rue rue1 = new Rue("Andriatsihoarana", HEI, Pullman);
+         Rue rue2 = new Rue("Ranaivo", Pullman, Balancoire);
+         Rue rue3 = new Rue("Rue3", Balancoire, ESTI);
+         Rue rue4 =  new Rue("rue4",Pullman,NextA);
+         Rue rue5 = new Rue("Rue5",Balancoire,Boulevard);
+         Rue rue6 = new Rue("Rue6",Boulevard,ESTI);
+         Rue rue7 = new Rue("Rue7",HEI,Sekolintsika);
+         Rue rue8 = new Rue("Rue8",Sekolintsika,Marais);
+         Rue rue9 = new Rue("Rue9",HEI,Balancoire);
+ 
+         // Créer la carte
+         Carte carte = new Carte();
+         carte.ajouterLieu(HEI);
+         carte.ajouterLieu(Pullman);
+         carte.ajouterLieu(Balancoire);
+         carte.ajouterLieu(ESTI);
+         carte.ajouterLieu(NextA);
+         carte.ajouterLieu(Boulevard);
+         carte.ajouterLieu(Sekolintsika);
+         carte.ajouterLieu(Marais);
+         carte.ajouterRue(rue1);
+         carte.ajouterRue(rue2);
+         carte.ajouterRue(rue3);
+         carte.ajouterRue(rue4);
+         carte.ajouterRue(rue5);
+         carte.ajouterRue(rue6);
+         carte.ajouterRue(rue7);
+         carte.ajouterRue(rue8);
+         carte.ajouterRue(rue9);
+ 
+         // Créer le marcheur
+         Marcheur bjarni = new Marcheur(carte);
+ 
+         // Faire marcher Bjarni vers ESTI
+         List<Lieu> parcours = bjarni.marcherVersDestination(HEI,ESTI);
+         // tester si la destination est ESTI
+         Assertions.assertEquals(ESTI, new LinkedList<>(parcours).getLast());
+         assertTrue(parcours.contains(Balancoire));
+    }
+}
