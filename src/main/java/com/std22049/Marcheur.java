8173a1ae7a55a08a4a021bbac5bc0f7b3762e4a5
Harenabs21 <hei.harena.6@gmail.com>
2024-06-18 16:58:02 +0300

    feat: ajouter l'attribut environnement et lieuxConnus

--- src/main/java/com/std22049/Marcheur.java
+++ src/main/java/com/std22049/Marcheur.java
@@ -1,19 +1,45 @@
 package com.std22049;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
+import java.util.Random;
+import java.util.Set;
+
+import com.std22049.cartes.Environnement;
 import com.std22049.cartes.Lieu;
+import com.std22049.cartes.Rue;
 
 import lombok.Data;
 
 @Data
 public class Marcheur {
     private String nom;
+    private Environnement environnement;
     private List<Lieu> lieuxDejaVisites;
+    private Set<Lieu> lieuxConnus;
     
-    public Marcheur(String nom){
+    public Marcheur(String nom, Environnement environnement){
         this.nom = nom;
+        this.environnement = environnement;
         this.lieuxDejaVisites = new ArrayList<>();
+        this.lieuxConnus = new HashSet<>();
+    }
+
+    public List<Lieu> marcherVersDestination(Lieu depart,Lieu destination){
+        Random random = new Random();
+        lieuxConnus.add(depart);
+        lieuxDejaVisites.add(depart);
+        while(!depart.equals(destination)){
+            List<Rue> ruesDisponibles = environnement.obtenirRuesDisponibles(depart);
+            Rue rueChoisie = ruesDisponibles.get(random.nextInt(ruesDisponibles.size()));
+            Lieu prochainLieu = rueChoisie.obtenirAutreLieu(depart);
+            depart = prochainLieu;
+            lieuxConnus.add(depart);
+            lieuxDejaVisites.add(depart);   
+        }
+        return lieuxDejaVisites;
+
     }
 }
